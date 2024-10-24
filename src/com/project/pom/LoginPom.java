6ed5be3768ed0628dcd492b3d072d1625e6d9253
SambitAutomation <sambit.nayak@webskitters.in>
2024-10-24 15:52:55 +0530

    Con

--- src/com/project/pom/LoginPom.java
+++ src/com/project/pom/LoginPom.java
@@ -1,5 +1,28 @@
 package com.project.pom;
 
-public class LoginPom{
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.project.generic.FileLib;
+
+public class LoginPom{
+	@FindBy(id="username")
+	private WebElement untbx;
+	
+	@FindBy(name="pwd")
+	private WebElement pwtbx;
+	
+	@FindBy(xpath="//div[text()='Login ']")
+	private WebElement lgBtn;
+	
+	public LoginPom(WebDriver driver) {
+	PageFactory.initElements(driver, this);
+	}
+	public void setLogin(String un,String pw) {
+	untbx.sendKeys(un);
+	pwtbx.sendKeys(pw);
+	lgBtn.click();		
+	}	
 }
